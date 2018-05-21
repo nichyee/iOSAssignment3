b4b9fb367b1f109835c1b341db1e3ea021609fee
Ronnie El-Alam <ronnieel-alam@ronnies-mac.local>
2018-05-21 05:08:41 -0700

    Camera should launch on game screen Commented the code to run the music since it was missing

---
+++ 360_NoScope/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  360_NoScope
+//
+//  Created by Ronnie El-Alam on 21/5/18.
+//  Copyright © 2018 Ronnie El-Alam. All rights reserved.
+//
+
+import Foundation
+import ARKit
+
+class GameViewController: UIViewController{
+    
+    var sceneView: ARSKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as? ARSKView? {
+            sceneView = view
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
