1595fece72a6990e2b9f73ed6538909823480ccb
Ronnie El-Alam <12596476@student.uts.edu.au>
2018-05-22 09:39:52 +1000

    Added game scene base class

---
+++ 360_NoScope/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  360_NoScope
+//
+//  Created by Ronnie El-Alam on 22/5/18.
+//  Copyright © 2018 Ronnie El-Alam. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import ARKit
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+}
